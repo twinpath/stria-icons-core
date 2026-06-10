13abf16ca368ba9f2337ed66835ea9abbbf1a9fe
dyzulk <66510723+dyzulk@users.noreply.github.com>
2026-06-10 15:07:27 +0700

    initial release: core assets

---
+++ dist/types/index.d.ts
@@ -0,0 +1,12 @@
+export interface IconData {
+  paths: string | string[];
+  viewBox: string;
+  unicode: string;
+}
+
+export declare const solid: Record<string, IconData>;
+export declare const regular: Record<string, IconData>;
+export declare const light: Record<string, IconData>;
+export declare const thin: Record<string, IconData>;
+export declare const duotone: Record<string, IconData>;
+export declare const brands: Record<string, IconData>;
